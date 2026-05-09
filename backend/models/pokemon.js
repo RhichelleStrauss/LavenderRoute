d435fb04d6def3c8f3b5e2a0755232e7ad05fb16
RhichelleStrauss <251169@virtualwindow.co.za>
2026-05-09 22:25:16 +0200

    models setup
    setting up models for pokemon and all fields in schema

--- backend/models/pokemon.js
+++ backend/models/pokemon.js
@@ -2,11 +2,57 @@ const mongoose = require("mongoose");
 
 const PokemonSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true, trim: true },
-        breed: { type: String, required: true, trim: true},
-        age: { type: Number, required: true, min: 0 },
-        colour: { type: String, default: 'Unkown', trim: true},
-        vaccinated: { type: Boolean, default: false },
+        name: {type: String, 
+                required: true, 
+                trim: true },
+
+        breed: {type: String, 
+                required: true, 
+                trim: true},
+
+        age: {type: Number, 
+                required: true, 
+                min: 0 },
+
+        colour: {type: String, 
+                default: 'Unkown', 
+                trim: true},
+
+        shiny: {type: Boolean, 
+                default: false },
+
+        description: {type: String, 
+                        required: true, 
+                        trim: true},
+
+        height: {type: Number, 
+                required: true, 
+                min: 0},
+
+        weight: {type: Number, 
+                required: true,
+                min: 0},
+
+        price: {type: Number,
+                required: true,
+                min: 0},
+
+        level: {type: Number,
+                required: true,
+                min: 0,
+                max: 100,
+                default: 1},
+        
+        gender: {type: String,
+                required: true,
+                enum: ['Male', 'Female', 'Genderless'],
+                default: 'Genderless' },
+        
+        type: {type: String,
+                required: true,
+                trim: true
+        }
+
     },
     {timestamps : true}
 );
