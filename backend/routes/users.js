f911bffc426dfd19e54333d05cb62f8538bb16b3
Nell-JvR-251061 <251061@virtualwindow.co.za>
2026-05-10 21:15:50 +0200

    User module & User route

---
+++ backend/routes/users.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        const saved = await newUser.save();
+
+        res.status(201).json(saved);
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try{
+        const users = await User.find();
+
+        res.status(201).json(users);
+    }
+    catch (error){
+        res.status(500).json({ message: error.message });
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);;
+
+        if(!user){
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json(user);
+    }
+    catch (error){
+        res.status(500).json({ message: error.message });
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try{
+        const updateUser = await User.findByIdAndUpdate(
+            req.params.id, 
+            req.body,
+        {
+            new: true,
+            runValidators: true,
+        });
+
+        if(!updateUser){
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json(updateUser);
+    }
+    catch (error){
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const deleteUser = await User.findByIdAndDelete(req.params.id);
+
+        if(!deleteUser){
+            res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ message: "User deleted successfully" });
+    }
+    catch (error){
+        res.status(400).json({ message: error.message });
+    }
+});
+
+module.exports = router;
