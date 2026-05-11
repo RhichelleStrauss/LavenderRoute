4428bb847de8dc5358543792fc5ab6997e345bbb
RhichelleStrauss <251169@virtualwindow.co.za>
2026-05-11 21:47:20 +0200

    added liquid ether background
    added liquid ether background, sign up page and product add page

--- frontend/LavendarRoute/src/App.jsx
+++ frontend/LavendarRoute/src/App.jsx
@@ -4,11 +4,16 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+import LiquidEther from '../src/components/LiquidEther';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
+    
+      
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
